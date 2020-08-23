26850574689fb680f80b55d5ea41ddecae60ebf8
dlwnsdyd94 <ljy1237@yonsei.ac.kr>
2020-08-23 20:04:56 +0900

    Reorder List 문제와 문제 풀이 설명 업데이트
    코드상에서 바로 문제가 무엇인지, 또 어떻게 풀었는 지 볼 수 있도록 수정하였다.

--- Reorder_List.cpp
+++ Reorder_List.cpp
@@ -1,3 +1,22 @@
+/* Reorder List 문제
+
+Given a singly linked list L: L0→L1→…→Ln-1→Ln,
+reorder it to: L0→Ln→L1→Ln-1→L2→Ln-2→…
+
+You may not modify the values in the list's nodes, only nodes itself may be changed.
+
+Example 1:
+Given 1->2->3->4, reorder it to 1->4->2->3.
+
+Example 2:
+Given 1->2->3->4->5, reorder it to 1->5->2->4->3.
+*/
+
+/*
+* 처음엔 Python으로 풀려했으나, Python에는 Linked List를 지원해주는 함수가 없으며 주소 접근에도 불편하다.
+* 따라서 C++이라는 언어로 변경하여 Pointer를 사용하여 풀었다.
+*/
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
